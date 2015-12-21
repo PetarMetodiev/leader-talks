1199d29e9e6cd8118f3f18de6f0e03210dfc8e12
twick <petar.metodiev@gmail.com>
2015-12-21 15:18:41 +0000

    Pushing all users at once to the client.

--- leader-talks/routes/controllers/utils/manageUsers.js
+++ leader-talks/routes/controllers/utils/manageUsers.js
@@ -4,37 +4,54 @@ var mongoose = require('mongoose');
 var User = require('../../../models/user');
 
 function returnPaginatedBlogposts(req, res, next) {
-    var elementsOnPage = 5;
-    var page = req.params.page || 1;
-    var elementsToSkip = (page - 1) * elementsOnPage;
-    var maxPage;
+    // var elementsOnPage = 5;
+    // var page = req.params.page || 1;
+    // var elementsToSkip = (page - 1) * elementsOnPage;
+    // var maxPage;
 
-    User.count({}, function(err, count) {
-        maxPage = Math.ceil(count / elementsOnPage);
+    // User.count({}, function(err, count) {
+    //     maxPage = Math.ceil(count / elementsOnPage);
 
-        if (page < 1 || page > maxPage) {
-            res.redirect('./');
-        }
+    //     if (page < 1 || page > maxPage) {
+    //         res.redirect('./');
+    //     }
 
-        User
-            .find({})
-            .sort({
-                'addedOn': -1
-            })
-            .limit(elementsOnPage)
-            .skip(elementsToSkip)
-            .exec(function(err, users) {
+    //     User
+    //         .find({})
+    //         .sort({
+    //             'addedOn': -1
+    //         })
+    //         .limit(elementsOnPage)
+    //         .skip(elementsToSkip)
+    //         .exec(function(err, users) {
+    //             if (err) {
+    //                 return next(err);
+    //             }
+
+    //             res.render('./templates/manageUsers', {
+    //                 users: users,
+    //                 currentPage: page,
+    //                 maxPage: maxPage
+    //             });
+    //         });
+    // });
+
+    User
+        .find({})
+        .sort({
+            'email': -1
+        })
+        .exec(
+            function(err, users){
                 if (err) {
                     return next(err);
                 }
 
-                res.render('./templates/manageUsers', {
-                    users: users,
-                    currentPage: page,
-                    maxPage: maxPage
-                });
-            });
-    });
+                res.render('./templates/manageUsers',{
+                    users: users
+                })
+            }
+        )
 
 
 }
